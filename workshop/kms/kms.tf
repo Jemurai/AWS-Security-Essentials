4a6b6e40b947e1ab455aee41a40705d094b4252f
Aaron Bedra <aaron@aaronbedra.com>
2017-11-23 14:48:07 -0600

    Complete integrated key example

--- workshop/kms/kms.tf
+++ workshop/kms/kms.tf
@@ -10,3 +10,15 @@ resource "aws_kms_alias" "master_alias" {
     name = "alias/master"
     target_key_id = "${aws_kms_key.master.key_id}"
 }
+
+resource "aws_dynamodb_table" "data_encryption_keys" {
+    name = "data_encryption_keys"
+    read_capacity = 1
+    write_capacity = 1
+    hash_key = "KeyId"
+
+    attribute {
+        name = "KeyId"
+        type = "S"
+    }
+}
