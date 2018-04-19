540ee96a75dea7c99868439b24618a2016e73778
Aaron Bedra <aaron@aaronbedra.com>
2018-04-19 15:06:58 -0500

    Updating Audit module. Closes #4, closes #6, closes #7

--- workshop/auditing/setup.tf
+++ workshop/auditing/setup.tf
@@ -1,20 +1,20 @@
 provider "aws" {
-    region = "ap-southeast-2"
+  region = "us-east-2"
 }
 
 terraform {
-    backend "s3" {
-        bucket = "abedra-yow-tfstate"
-        key = "auditing/terraform.tfstate"
-        region = "ap-southeast-2"
-    }
+  backend "s3" {
+    bucket = "abedra-goto-tfstate"
+    key = "auditing/terraform.tfstate"
+    region = "us-east-2"
+  }
 }
 
-resource "aws_s3_bucket" "abedra-yow-audit" {
-    bucket = "abedra-yow-audit"
-    force_destroy = true
+resource "aws_s3_bucket" "abedra-goto-audit" {
+  bucket = "abedra-goto-audit"
+  force_destroy = true
 
-    policy = <<POLICY
+  policy = <<POLICY
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -25,7 +25,7 @@ resource "aws_s3_bucket" "abedra-yow-audit" {
               "Service": "cloudtrail.amazonaws.com"
             },
             "Action": "s3:GetBucketAcl",
-            "Resource": "arn:aws:s3:::abedra-yow-audit"
+            "Resource": "arn:aws:s3:::abedra-goto-audit"
         },
         {
             "Sid": "AWSCloudTrailWrite",
@@ -34,7 +34,7 @@ resource "aws_s3_bucket" "abedra-yow-audit" {
               "Service": "cloudtrail.amazonaws.com"
             },
             "Action": "s3:PutObject",
-            "Resource": "arn:aws:s3:::abedra-yow-audit/*",
+            "Resource": "arn:aws:s3:::abedra-goto-audit/*",
             "Condition": {
                 "StringEquals": {
                     "s3:x-amz-acl": "bucket-owner-full-control"
@@ -47,8 +47,12 @@ POLICY
 }
 
 resource "aws_cloudtrail" "audit" {
-    name                          = "audit"
-    s3_bucket_name                = "${aws_s3_bucket.abedra-yow-audit.id}"
-    s3_key_prefix                 = "audit"
-    include_global_service_events = false
+  name                          = "audit"
+  s3_bucket_name                = "${aws_s3_bucket.abedra-goto-audit.id}"
+  s3_key_prefix                 = "audit"
+  include_global_service_events = true
+}
+
+resource "aws_guardduty_detector" "GOTO" {
+  enable = true
 }
